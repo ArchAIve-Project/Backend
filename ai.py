aa5a474f00ea603c87976049bab97618906c6c72
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2025-06-19 22:45:26 +0800

    created Tool and InteractionContext representational classes

--- ai.py
+++ ai.py
@@ -1,13 +1,17 @@
 import os, sys, openai, base64
+from typing import List
 from enum import Enum
 from openai import OpenAI
-from openai.types.chat import ChatCompletionMessage
+from openai.types.chat import ChatCompletionMessage, ChatCompletionMessageToolCall
 
 class LMProvider(str, Enum):
     OPENAI = "openai"
     QWEN = "qwen"
+    
+    def __str__(self):
+        return self.value
 
-class LMVariants(str, Enum):
+class LMVariant(str, Enum):
     GPT_4O = "gpt-4o"
     GPT_4O_MINI = "gpt-4o-mini"
     GPT_4_1 = "gpt-4.1"
@@ -33,6 +37,9 @@ class LMVariants(str, Enum):
     QWEN3_32B = "qwen3-32b"
     QWEN3_30B_A3B = "qwen3-30b-a3b"
     QWEN3_235B_A22B = "qwen3-235b-a22b"
+    
+    def __str__(self):
+        return self.value
 
 class ClientConfig:
     def __init__(self, name: str, *args, **kwargs):
@@ -47,7 +54,7 @@ class ClientConfig:
         return f"ClientConfig(name={self.name}, args={self.args}, kwargs={self.kwargs})"
     
     @staticmethod
-    def default() -> 'list[ClientConfig]':
+    def default() -> 'List[ClientConfig]':
         return [
             ClientConfig("openai"),
             ClientConfig(
@@ -63,8 +70,8 @@ class Interaction:
         ASSISTANT = "assistant"
         SYSTEM = "system"
         TOOL = "tool"
-    
-    def __init__(self, role: Role | str, content: str, imagePath: str | None=None, imageFileType: str | None=None):
+
+    def __init__(self, role: Role | str, content: str, imagePath: str | None=None, imageFileType: str | None=None, completionMessage: ChatCompletionMessage | None=None):
         if (not isinstance(role, str) and not isinstance(role, Interaction.Role)):
             raise ValueError("Role must be a string or an instance of Interaction.Role.")
         elif not isinstance(content, str):
@@ -75,6 +82,8 @@ class Interaction:
             raise ValueError("Image file type must be a string if provided.")
         elif imagePath is not None and imageFileType is None:
             raise ValueError("Image file type must be provided if image path is given.")
+        elif imagePath is not None and str(role) != Interaction.Role.USER.value:
+            raise ValueError("Image input can only be included by the user role.")
         
         self.role: str = role if isinstance(role, str) else role.value
         self.content: str = content
@@ -86,6 +95,7 @@ class Interaction:
         else:
             self.imageData: str = None
         self.imageFileType: str = imageFileType
+        self.completionMessage: ChatCompletionMessage | None = completionMessage
 
     def represent(self):
         if self.imageData is None:
@@ -109,6 +119,90 @@ class Interaction:
                     }
                 ]
             }
+    
+    def __str__(self):
+        return f"""Interaction:
+    Role: {self.role}
+    Content: {self.content}
+    Image Input: {'Yes' if self.imageData else 'No'}
+    Image File Type: {self.imageFileType if self.imageFileType else 'None'}"""
+
+class Tool:
+    class Parameter:
+        class Type(str, Enum):
+            STRING = "string"
+            INTEGER = "integer"
+            NUMBER = "number"
+            BOOLEAN = "boolean"
+            ARRAY_NOT_RECOMMENDED = "array"
+            OBJECT_NOT_RECOMMENDED = "object"
+
+            def __str__(self):
+                return self.value
+        
+        def __init__(self, name: str, dataType: Type, description: str, required: bool=False):
+            self.name = name
+            self.type = str(dataType)
+            self.description = description
+            self.required = required
+        
+    def __init__(self, callback, name: str, description: str, parameters: List[Parameter] | None=None):
+        if not callable(callback):
+            raise ValueError("Callback must be a callable function.")
+        
+        self.callback = callback
+        self.name = name
+        self.description = description
+        self.parameters = parameters
+    
+    def invoke(self, *args, **kwargs):
+        return self.callback(*args, **kwargs)
+    
+    def represent(self) -> dict:
+        data = {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description
+            }
+        }
+        
+        if self.parameters is not None and isinstance(self.parameters, list) and len(self.parameters) > 0:
+            data["function"]["parameters"] = {
+                "type": "object",
+                "properties": {param.name: {
+                    "type": param.type,
+                    "description": param.description
+                } for param in self.parameters},
+                "required": [param.name for param in self.parameters if param.required]
+            }
+
+        return data
+        
+
+class InteractionContext:
+    def __init__(
+        self,
+        provider: LMProvider,
+        variant: LMVariant,
+        history: List[Interaction]=[],
+        tools: List[Tool]=[],
+        temperature: float=0.5,
+        presence_penalty: float | None=None,
+        top_p: float | None=None,
+        top_k: int | None=None,
+    ):
+        self.provider = provider
+        self.variant = variant
+        self.history = history
+        self.tools = tools
+        self.temperature = temperature
+        self.presence_penalty = presence_penalty
+        self.top_p = top_p
+        self.top_k = top_k
+    
+    def addInteraction(self, interaction: Interaction):
+        self.history.append(interaction)
 
 class LLMInterface:
     clients: dict[str, OpenAI] = {}
