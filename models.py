1d352263e9ffd6d4c0100d15937d929093d1f455
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2025-06-30 14:55:25 +0800

    added models.py, beginning User model work

---
+++ models.py
@@ -0,0 +1,69 @@
+from database import DI, DIRepresentable, DIError
+from utils import Ref
+from services import Universal
+
+class User(DIRepresentable):
+    def __init__(self, username: str, email: str, pwd: str, authToken: str=None, superuser: bool=False, lastLogin: str=None, created: str=None, id: str=None):
+        if id is None:
+            id = Universal.generateUniqueID()
+        if created is None:
+            created = Universal.utcNowString()
+        
+        self.id = id
+        self.username = username
+        self.email = email
+        self.pwd = pwd
+        self.authToken = authToken
+        self.superuser = superuser
+        self.lastLogin = lastLogin
+        self.created = created
+    
+    @staticmethod
+    def rawLoad(data):
+        requiredParams = ['username', 'email', 'pwd', 'authToken', 'superuser', 'lastLogin', 'created', 'id']
+        for reqParam in requiredParams:
+            if reqParam not in data:
+                if reqParam in []: # add any required params that should be empty dictionaries
+                    data[reqParam] = {}
+                elif reqParam in []: # add any required params that should be empty lists
+                    data[reqParam] = []
+                elif reqParam in ['superuser']:
+                    data[reqParam] = False
+                else:
+                    data[reqParam] = None
+        
+        if not isinstance(data['superuser'], bool):
+            data['superuser'] = False
+        
+        return User(
+            username=data['username'],
+            email=data['email'],
+            pwd=data['pwd'],
+            authToken=data['authToken'],
+            superuser=data['superuser'],
+            lastLogin=data['lastLogin'],
+            created=data['created'],
+            id=data['id']
+        )
+    
+    @staticmethod
+    def load(id: str=None, username: str=None, email: str=None):
+        if id is None and username is None and email is None:
+            raise Exception("USER LOAD ERROR: No identifier provided.")
+        
+        if id != None:
+            data = DI.load(User.ref(id))
+            if isinstance(data, DIError):
+                raise Exception("USER LOAD ERROR: DIError occurred: {}".format(data))
+            if data == None:
+                return None
+            if not isinstance(data, dict):
+                raise Exception("USER LOAD ERROR: Unexpected DI load response format; response: {}".format(data))
+            
+            return User.rawLoad(data)
+        else:
+            pass # TODO
+    
+    @staticmethod
+    def ref(id: str) -> Ref:
+        return Ref("user", id)
