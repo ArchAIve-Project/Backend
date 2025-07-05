7ebef31d6c3fb80d41f8df65ccd0eee90aa1828b
ZacTohZY <234453p@mymail.nyp.edu.sg>
2025-07-06 01:04:09 +0800

    Proper use of Dict and dict

--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 from database import DI, DIRepresentable, DIError
 from utils import Ref
 from services import Universal
-from typing import List, Dict
+from typing import List, Dict, Any
 
 class Artefact(DIRepresentable):
-    def __init__(self, name: str, image: str, metadata: 'Metadata | Dict | None', public: bool = False, created: str=None, id: str = None):
+    def __init__(self, name: str, image: str, metadata: 'Metadata | Dict[str, Any] | None', public: bool = False, created: str=None, id: str = None):
         if id is None:
             id = Universal.generateUniqueID()
         if created is None:
             created = Universal.utcNowString()
-        if isinstance(metadata, Dict):
+        if isinstance(metadata, dict):
             metadata = Metadata.fromMetagen(id, metadata)
         if not isinstance(metadata, Metadata):
             metadata = Metadata(artefactID=id)
@@ -28,7 +28,7 @@ class Artefact(DIRepresentable):
     def destroy(self):
         return DI.save(None, self.originRef)
 
-    def represent(self) -> Dict:
+    def represent(self) -> Dict[str, Any]:
         return {
             'id': self.id,
             'public': self.public,
@@ -49,7 +49,7 @@ class Artefact(DIRepresentable):
                     data[param] = None
 
         metadata = None
-        if isinstance(data['metadata'], Dict):
+        if isinstance(data['metadata'], dict):
             metadata = Metadata.rawLoad(data['id'], data['metadata'])
 
         output = Artefact(
@@ -71,7 +71,7 @@ class Artefact(DIRepresentable):
                 raise Exception("ARTEFACT LOAD ERROR: DIError occurred: {}".format(data))
             if data is None:
                 return None
-            if not isinstance(data, Dict):
+            if not isinstance(data, dict):
                 raise Exception("ARTEFACT LOAD ERROR: Unexpected DI load response format; response: {}".format(data))
             
             return Artefact.rawLoad(data)
@@ -81,12 +81,12 @@ class Artefact(DIRepresentable):
                 return None
             if isinstance(data, DIError):
                 raise Exception("ARTEFACT LOAD ERROR: DIError occurred: {}".format(data))
-            if not isinstance(data, Dict):
+            if not isinstance(data, dict):
                 raise Exception("ARTEFACT LOAD ERROR: Failed to load dictionary artefacts data; response: {}".format(data))
 
             artefacts: Dict[str, Artefact] = {}
             for id in data:
-                if isinstance(data[id], Dict):
+                if isinstance(data[id], dict):
                     artefacts[id] = Artefact.rawLoad(data[id])
             
             return list(artefacts.values())
@@ -126,7 +126,7 @@ class Metadata(DIRepresentable):
         return isinstance(self.raw, HFData)
     
     @staticmethod
-    def rawLoad(artefactID: str, data: Dict) -> 'Metadata':
+    def rawLoad(artefactID: str, data: Dict[str, Any]) -> 'Metadata':
         if 'tradCN' in data:
             return Metadata(artefactID, MMData.rawLoad(data, artefactID))
         elif 'faceFiles' in data:
@@ -141,13 +141,13 @@ class Metadata(DIRepresentable):
             return None
         if isinstance(data, DIError):
             raise Exception("METADATA LOAD ERROR: DIError occurred: {}".format(data))
-        if not isinstance(data, Dict):
+        if not isinstance(data, dict):
             raise Exception("METADATA LOAD ERROR: Unexpected DI load response format; response: {}".format(data))
 
         return Metadata.rawLoad(artefactID, data)
 
     @staticmethod
-    def fromMetagen(artefactID: str, metagenDict: Dict) -> 'Metadata':
+    def fromMetagen(artefactID: str, metagenDict: dict) -> 'Metadata':
         # 1. identify whether it is mm data or hf data
         # 2. manually feed in the values to either constructor to product a MMData | HFData object
         # 3. Pass the data object to Metadata constructor
@@ -199,7 +199,7 @@ class MMData(DIRepresentable):
     def destroy(self):
         return DI.save(None, self.originRef)
 
-    def represent(self) -> Dict[str, any]:
+    def represent(self) -> Dict[str, Any]:
         return {
             "tradCN": self.tradCN,
             "corrected": self.corrected,
@@ -212,7 +212,7 @@ class MMData(DIRepresentable):
         }
 
     @staticmethod
-    def rawLoad(data: Dict[str, any], artefactID: str) -> 'MMData':
+    def rawLoad(data: Dict[str, Any], artefactID: str) -> 'MMData':
         requiredParams = ['tradCN', 'corrected','preCorrectionAcc','simplifiedCN','english','summary','nerLabels']
         for reqParam in requiredParams:
             if reqParam not in data:
@@ -243,7 +243,7 @@ class MMData(DIRepresentable):
             return None
         if isinstance(data, DIError):
             raise Exception("MMDATA LOAD ERROR: DIError occurred: {}".format(data))
-        if not isinstance(data, Dict):
+        if not isinstance(data, dict):
             raise Exception("MMDATA LOAD ERROR: Unexpected DI load response format; response: {}".format(data))
 
         return MMData.rawLoad(data, artefactID)
@@ -262,14 +262,14 @@ class HFData(DIRepresentable):
     def save(self) -> bool:
         return DI.save(self.represent(), self.originRef)
 
-    def represent(self) -> Dict[str, any]:
+    def represent(self) -> Dict[str, Any]:
         return {
             "faceFiles": self.faceFiles,
             "caption": self.caption
         }
 
     @staticmethod
-    def rawLoad(data: Dict[str, any], artefactID: str) -> 'HFData':
+    def rawLoad(data: Dict[str, Any], artefactID: str) -> 'HFData':
         requiredParams = ['faceFiles', 'caption']
         for reqParam in requiredParams:
             if reqParam not in data:
@@ -291,7 +291,7 @@ class HFData(DIRepresentable):
             return None
         if isinstance(data, DIError):
             raise Exception("HFDATA LOAD ERROR: DIError occurred: {}".format(data))
-        if not isinstance(data, Dict):
+        if not isinstance(data, dict):
             raise Exception("HFDATA LOAD ERROR: Unexpected DI load response format; response: {}".format(data))
 
         return HFData.rawLoad(data, artefactID)
