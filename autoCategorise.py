a71962d19282247e06be9f5417e100ff6781662b
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2025-08-13 05:55:25 +0800

    prompt feeding into LLM kind of works, but more testing needed due to low categories

--- autoCategorise.py
+++ autoCategorise.py
@@ -10,6 +10,26 @@ class CategorisationPrompt:
     def load():
         with open("templates/llmCategorisationPrompt.txt", "r") as f:
             CategorisationPrompt.text = f.read()
+    
+    @staticmethod
+    def fill(art: Artefact, categories: List[Category]):
+        categories = categories or []
+        if not CategorisationPrompt.text:
+            return None
+        
+        filledText = CategorisationPrompt.text
+        
+        existingCategories = ['- {}: {}'.format(c.name, c.description) for c in categories]
+        existingCategories = "\n".join(existingCategories) or "No categories yet"
+        
+        filledText = filledText.format(
+            existingCategories=existingCategories,
+            artName=art.name,
+            artCaption=art.metadata.raw.caption,
+            artAddInfo=art.metadata.raw.addInfo
+        )
+        
+        return filledText
 
 class AutoCategoriser:
     @staticmethod
@@ -18,18 +38,27 @@ class AutoCategoriser:
             provider=LMProvider.QWEN,
             variant=LMVariant.QWEN_VL_PLUS
         )
-
+        
+        if not CategorisationPrompt.text:
+            CategorisationPrompt.load()
+        prompt = CategorisationPrompt.fill(art, categories)
+        if not prompt:
+            print("Error: Failed to generate prompt")
+            return
+        
         cont.addInteraction(
             Interaction(
                 role=Interaction.Role.USER,
-                content="TBC"
+                content=prompt
             )
         )
 
         out = LLMInterface.engage(cont)
 
-        print(out.content)
+        return out.content
     
     @staticmethod
     def feed(art: Artefact):
-        pass
+        categories = Category.load()
+        
+        return AutoCategoriser.getLLMCategorisation(art, categories)
